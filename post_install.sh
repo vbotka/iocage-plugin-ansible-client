4dc094ef02ba72b4dcde019688abc17566208ed4
Vladimir Botka <vbotka@gmail.com>
2026-05-02 12:23:52 +0200

    Put the environment into /root/PLUGIN_INFO

--- post_install.sh
+++ post_install.sh
@@ -5,3 +5,5 @@ pw add user -n admin -c Administrator -s /bin/sh -m
 
 # Configure passwordless sudo for admin
 echo "admin ALL=(ALL) NOPASSWD: ALL" > /usr/local/etc/sudoers.d/admin
+
+echo $(printenv) > /root/PLUGIN_INFO
