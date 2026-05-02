f1c8a82d8b0358089d8d1057598e0b694f4c9a87
Vladimir Botka <vbotka@gmail.com>
2026-05-02 13:28:15 +0200

    Update test.

--- post_install.sh
+++ post_install.sh
@@ -1,9 +1,31 @@
 #!/bin/sh
 
-# Create user 'admin'
-pw add user -n admin -c Administrator -s /bin/sh -m
+plugin_name_full=$(hostname)
+plugin_name=$(echo "$str" | cut -d'_' -f1)
+plugin_no=$(echo "$str" | cut -d'_' -f2)
 
-# Configure passwordless sudo for admin
-echo "admin ALL=(ALL) NOPASSWD: ALL" > /usr/local/etc/sudoers.d/admin
+case "$plugin_name" in
+    ansible-test)
+	cat << EOF > /root/PLUGIN_INFO
+plugin_name: $plugin_name
+plugin_name_full: $plugin_name_full
+plugin_no: $plugin_no
+plugin_ip: $IOCAGE_PLUGIN_IP
 
-echo $(printenv) > /root/PLUGIN_INFO
+IP: $%%IP%%
+JAIL_NAME: $%%JAIL_NAME%%
+POOL_PATH: $%%POOL_PATH%%
+TAG: $%%TAG%%
+EOF
+	;;
+    ansible-zero)
+	pw add user -n admin -c Administrator -s /bin/sh -m
+        echo "admin ALL=(ALL) NOPASSWD: ALL" > /usr/local/etc/sudoers.d/admin
+	cat << EOF > /root/PLUGIN_INFO
+plugin_name: $plugin_name
+plugin_name_full: $plugin_name_full
+plugin_no: $plugin_no
+plugin_ip:
+EOF
+	;;
+esac
