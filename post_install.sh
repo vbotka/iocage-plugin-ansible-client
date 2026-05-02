399218b3f8ad4206ae3db359dbe2645e9cebb197
Vladimir Botka <vbotka@gmail.com>
2026-05-02 14:04:32 +0200

    Update test.

--- post_install.sh
+++ post_install.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
 plugin_name_full=$(hostname)
-plugin_name="${plugin_name_full%:*}"
-plugin_no="${$plugin_name_full#*:}"
+
+IFS="_" read -r plugin_name plugin_no << EOF
+$plugin_name_full
+EOF
 
 cat << EOF > /root/PLUGIN_INFO
 plugin_name: "$plugin_name"
 plugin_name_full: "$plugin_name_full"
 plugin_no: "$plugin_no"
 plugin_ip: "$IOCAGE_PLUGIN_IP"
-
-IP: "$IP"
-JAIL_NAME: "$JAIL_NAME"
-POOL_PATH: "$POOL_PATH"
-TAG: "$TAG"
 EOF
