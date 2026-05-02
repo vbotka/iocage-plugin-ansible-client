9d9375e01970c51a0d36eb5f54f45d560e9e6980
Vladimir Botka <vbotka@gmail.com>
2026-05-02 13:37:49 +0200

    Update test.

--- post_install.sh
+++ post_install.sh
@@ -4,9 +4,7 @@ plugin_name_full=$(hostname)
 plugin_name=$(echo "$str" | cut -d'_' -f1)
 plugin_no=$(echo "$str" | cut -d'_' -f2)
 
-case "$plugin_name" in
-    ansible-test)
-	cat << EOF > /root/PLUGIN_INFO
+cat << EOF > /root/PLUGIN_INFO
 plugin_name: $plugin_name
 plugin_name_full: $plugin_name_full
 plugin_no: $plugin_no
@@ -17,15 +15,3 @@ JAIL_NAME: $%%JAIL_NAME%%
 POOL_PATH: $%%POOL_PATH%%
 TAG: $%%TAG%%
 EOF
-	;;
-    ansible-zero)
-	pw add user -n admin -c Administrator -s /bin/sh -m
-        echo "admin ALL=(ALL) NOPASSWD: ALL" > /usr/local/etc/sudoers.d/admin
-	cat << EOF > /root/PLUGIN_INFO
-plugin_name: $plugin_name
-plugin_name_full: $plugin_name_full
-plugin_no: $plugin_no
-plugin_ip:
-EOF
-	;;
-esac
