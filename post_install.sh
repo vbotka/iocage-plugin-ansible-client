fe211b86d298dc8cccc36317bd639d17c4f0429f
Vladimir Botka <vbotka@gmail.com>
2026-05-03 05:44:55 +0200

    Update test.

--- post_install.sh
+++ post_install.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
 plugin_name=$(hostname)
-iocage_pool=$(iocage get -p pool)
-iocage_pool_mountpoint=$(zfs get -H -o value mountpoint "$iocage_pool")
-iocage_plugins_dir="${iocage_pool_mountpoint}/iocage/.plugins/github_com_vbotka_iocage-plugins"
 
 case "$plugin_name" in
     ansible-test)
-	. "${iocage_plugins_dir}/post_install_ansible_test.sh"
+	cat << EOF > /root/PLUGIN_INFO
+plugin_name: $plugin_name
+plugin_ip: $IOCAGE_PLUGIN_IP
+EOF
 	;;
     ansible-zero)
-	. "${iocage_plugins_dir}/post_install_ansible_zero.sh"
+	cat << EOF > /root/PLUGIN_INFO
+plugin_name: $plugin_name
+plugin_ip: $IOCAGE_PLUGIN_IP
+EOF
 	;;
     syslog-ng)
-	. "${iocage_plugins_dir}/post_install_syslog_ng.sh"
+	cat << EOF > /root/PLUGIN_INFO
+plugin_name: $plugin_name
+plugin_ip: $IOCAGE_PLUGIN_IP
+EOF
 	;;
 esac
--- post_install_ansible_zero.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cat << EOF > /root/PLUGIN_INFO
-plugin_name: $plugin_name
-plugin_ip: $IOCAGE_PLUGIN_IP
-EOF
