dd80f8d540a27cc1f99981f667c05ad0fbf43efa
Vladimir Botka <vbotka@gmail.com>
2026-05-05 05:56:00 +0200

    Update post_install.

--- post_install.sh
+++ post_install.sh
@@ -5,6 +5,16 @@ ansible_custom_facts_dir="/etc/ansible/facts.d"
 
 case "$plugin_name" in
     ansible-syslogng)
+	mkdir -p "$ansible_custom_facts_dir"
+	cat << EOF > "${ansible_custom_facts_dir}/iocage.fact"
+#!/bin/sh
+cat << EOF2
+{
+  "iocage_plugin_name": "ansible-syslogng"
+}
+EOF2
+EOF
+	chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
 	;;
     ansible-test)
 	cat << EOF > /root/PLUGIN_INFO
