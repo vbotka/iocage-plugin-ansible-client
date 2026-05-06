ef9bad3ba70c2e78cc7db31187e65c05f01e4638
Vladimir Botka <vbotka@gmail.com>
2026-05-07 01:41:16 +0200

    Remove ansible-syslogng-client and server.

--- post_install.sh
+++ post_install.sh
@@ -15,30 +15,7 @@ cat << EOF
 EOF
 EOF2
 	chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
-	;;
-    ansible-syslogng-client)
-	mkdir -p "$ansible_custom_facts_dir"
-	cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
-#!/bin/sh
-cat << EOF
-{
-  "iocage_plugin_name": "$plugin_name"
-}
-EOF
-EOF2
-	chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
-	;;
-    ansible-syslogng-server)
-	mkdir -p "$ansible_custom_facts_dir"
-	cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
-#!/bin/sh
-cat << EOF
-{
-  "iocage_plugin_name": "$plugin_name"
-}
-EOF
-EOF2
-	chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
+	echo "LC_ALL=en_US.UTF-8" >> /root/.profile
 	;;
     ansible-test)
 	cat << EOF > /root/PLUGIN_INFO
@@ -55,15 +32,9 @@ cat << EOF
 EOF
 EOF2
 	chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
+	echo "LC_ALL=en_US.UTF-8" >> /root/.profile
 	;;
     ansible-pull-test)
-	cat << EOF > /root/PLUGIN_INFO
-plugin_name: $plugin_name
-plugin_ip: $IOCAGE_PLUGIN_IP
-EOF
-	cat << EOF > /root/hosts
-localhost.my.domain ansible_connection=local
-EOF
 	mkdir -p "$ansible_custom_facts_dir"
 	cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
 #!/bin/sh
