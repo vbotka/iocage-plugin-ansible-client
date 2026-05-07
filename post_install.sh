eaf3500992fae5be3bc48b6b3cad180c7bc1c3f0
Vladimir Botka <vbotka@gmail.com>
2026-05-07 17:48:06 +0200

    Add plugin ansible-syslogng-client

--- post_install.sh
+++ post_install.sh
@@ -40,6 +40,25 @@ EOF2
 	chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
 	echo "LC_ALL=en_US.UTF-8" >> /root/.profile
 	;;
+    ansible-pull-syslogng-client)
+	# Create custom facts
+	mkdir -p "$ansible_custom_facts_dir"
+	cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
+#!/bin/sh
+cat << EOF
+{
+  "iocage_plugin_name": "$plugin_name"
+}
+EOF
+EOF2
+	chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
+	# Ansible needs UTF-8
+	echo "LC_ALL=en_US.UTF-8" >> /root/.profile
+	# Configure and start syslog-ng client
+	mkdir /root/ansible-conf-syslogng-client
+	cd /root/ansible-conf-syslogng-client
+	ansible-pull -i hosts -U https://github.com/vbotka/ansible-conf-syslogng-server.git -d /root/ansible-conf-syslogng-server pb-logclient.yml
+	;;
     ansible-pull-syslogng-server)
 	# Create custom facts
 	mkdir -p "$ansible_custom_facts_dir"
